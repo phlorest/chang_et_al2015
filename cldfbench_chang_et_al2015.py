d36de5badde1783defbc628d36a4304501c4d531
SimonGreenhill <simon@simon.net.nz>
2022-04-22 22:21:57 +1200

    update

--- cldfbench_chang_et_al2015.py
+++ cldfbench_chang_et_al2015.py
@@ -9,23 +9,15 @@ class Dataset(phlorest.Dataset):
 
     def cmd_makecldf(self, args):
         self.init(args)
-        args.writer.add_summary(
-            self.raw_dir.joinpath('a1-c0-d0-g1-l2-s1-t1-z8').read_tree(
-                'mcc.trees', detranslate=True),
-            self.metadata,
-            args.log)
-        posterior = self.sample(
-            self.remove_burnin(
-                self.raw_dir.joinpath('analyses').read('ieo.trees.gz'),
-                10001),
-            detranslate=True,
-            as_nexus=True)
+        
+        summary = self.raw_dir.read_tree('mcc.trees', detranslate=True)
+        args.writer.add_summary(summary, self.metadata, args.log)
 
-        args.writer.add_posterior(
-            posterior.trees.trees,
-            self.metadata,
-            args.log,
-            verbose=True)
+        posterior = self.raw_dir.read_trees(
+           'ieo.trees.gz',
+           burnin=10001, sample=1000, detranslate=True)
+        args.writer.add_posterior(posterior, self.metadata, args.log)
+        
         args.writer.add_data(
             self.raw_dir.read_nexus('a1-c0-d0-g1-l2-s1-t1-z8_ieo.nex'),
             self.characters,
